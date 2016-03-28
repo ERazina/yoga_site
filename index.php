61ead44db51329c904369f5d4d06053c91f856c1
Elina <razina_elina@mail.ru>
2016-03-28 12:05:14 +0300

    files added

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+define('PAGE', 'index');
+include_once "header.html";
+include_once "menu.php";
+include_once "footer.html";
+
+?>
+
+<p>Центр йоги "Аянгара" был открыт в 2011 году.</p>
+<div class="">
+  <p>Методика</p>
+  <p>Наша методика абсолютно безопасна</p>
+</div>
+<div class="">
+  <p>Цены</p>
+    <p>Наша методика абсолютно безопасна</p>
+</div>
+<div class="">
+  <p>Расписание</p>
+    <p>Наша методика абсолютно безопасна</p>
+</div>
---
+++ menu.php
@@ -0,0 +1,11 @@
+<nav>
+  <img src="" alt="шапка центра "Аянгара"" />
+    <ul>
+        <li <?php if (PAGE == 'index') echo 'class = "active"'; ?>><a href = "index.php">Главная</a></li>
+        <li <?php if (PAGE == 'about') echo 'class = "active"'; ?>><a href = "about.php">О нас</a></li>
+        <li <?php if (PAGE == 'prices') echo 'class = "active"'; ?>><a href = "prices.php">Цены</a></li>
+        <li <?php if (PAGE == 'schedule') echo 'class = "active"'; ?>><a href = "schedule.php">Расписание</a></li>
+        <li <?php if (PAGE == 'contacts') echo 'class = "active"'; ?>><a href = "contacts.php">Контакты</a></li>
+    </ul>
+
+</nav>
