3b416c411c222866c1380048ce0763774fc7dd81
Elina <razina_elina@mail.ru>
2016-04-19 09:38:43 +0300

    new pictures and css added
    looking better

--- index.php
+++ index.php
@@ -3,38 +3,44 @@
 define('PAGE', 'index');
 include_once "header.html";
 include_once "menu.php";
-//include_once "footer.html";
-
 ?>
 
 <h1>наши преимущества:</h1>
 <div id = "index-flexbox">
 <div class="index_block">
   <h2>методика</h2>
-    <ol class = "index_list">
+    <ul class = "index_list">
+        <img src="img/metodika.jpg" class = "index_image">
         <li class="index_li">Йога Айенгара – самая безопасная и эффективная методика</li>
         <li class="index_li">Йога для начинающих и продолжающих</li>
         <li class="index_li">Йога терапия. Занятия йогой для решения проблем со здоровьем.</li>
         <li class="index_li">Квалифицированные преподаватели.</li>
-   <ol> 
+    </ul> 
 </div>
 <div class="index_block">
   <h2>цены</h2>
-    <ol class = "index_list">
+    <img src="img/price3.jpg" class="index_image">
+    <ul class = "index_list">
         <li class="index_li">Доступные цены и акции</li>
         <li class="index_li">Занятия не пропадают. Не использованный остаток переноситься на любой следующий абонемент.</li>
         <li class="index_li">Рассрочка и большой срок действия абонементов.</li>
         <li class="index_li">Скидка при продлении абонемента.</li>
         <li class="index_li">Все доступные способы оплаты.</li>
-    </ol>
+    </ul>
 </div>
 <div class="index_block">
+    <img >
   <h2>расписание</h2>
-    <ol class = "index_list">
+    <ul class = "index_list">
         <li class="index_li">30 занятий в неделю.</li>
         <li class="index_li">Семь дней в неделю: Утро / День / Вечер.</li>
         <li class="index_li">Мини-группы до 6 человек.</li>
         <li class="index_li">Удобная запись</li>
-    </ol>
+    </ul>
 </div>
 </div>
+
+
+<?php
+include_once "footer.html";
+?>
