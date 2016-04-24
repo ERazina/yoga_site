d8365e363cb4e6e20bef9f820f7c1e1c75c1b7f7
Elina <razina_elina@mail.ru>
2016-04-24 20:58:43 +0300

    google map added + prices page

--- prices.php
+++ prices.php
@@ -5,6 +5,72 @@ include_once "menu.php";
 ?>
 
 
+<h1>С абонементом лучше!</h1>
+
+<p>Абонемент — это не только более доступный способ оплаты, но и простая возможность войти в ритм практики, занимаясь йогой регулярно.</p>
+
+<div id = "index-flexbox">
+<div class="index_block">
+  <h2>5 занятий</h2>
+    <ul class = "index_list">
+        <li class="index_li">Йога Айенгара – самая безопасная и эффективная методика</li>
+        <li class="index_li">Йога для начинающих и продолжающих</li>
+        <li class="index_li">Йога терапия. Занятия йогой для решения проблем со здоровьем.</li>
+        <li class="index_li">Квалифицированные преподаватели.</li>
+    </ul> 
+</div>
+<div class="index_block">
+  <img src="img/price3.jpg" class="index_image">
+    <h2>9 занятий</h2>
+    <ul class = "index_list">
+        <li class="index_li">Доступные цены и акции</li>
+        <li class="index_li">Занятия не пропадают. Не использованный остаток переноситься на любой следующий абонемент.</li>
+        <li class="index_li">Рассрочка и большой срок действия абонементов.</li>
+        <li class="index_li">Скидка при продлении абонемента.</li>
+        <li class="index_li">Все доступные способы оплаты.</li>
+    </ul>
+</div>
+<div class="index_block">
+<img src="img/yoga_clock.jpg" class = "index_image">
+  <h2>14 занятий</h2>
+    <ul class = "index_list">
+        <li class="index_li">30 занятий в неделю.</li>
+        <li class="index_li">Семь дней в неделю: Утро / День / Вечер.</li>
+        <li class="index_li">Мини-группы до 6 человек.</li>
+        <li class="index_li">Удобная запись</li>
+    </ul>
+</div>
+    <div class="index_block">
+<img src="img/yoga_clock.jpg" class = "index_image">
+  <h2>21 занятие</h2>
+    <ul class = "index_list">
+        <li class="index_li">30 занятий в неделю.</li>
+        <li class="index_li">Семь дней в неделю: Утро / День / Вечер.</li>
+        <li class="index_li">Мини-группы до 6 человек.</li>
+        <li class="index_li">Удобная запись</li>
+    </ul>
+</div>
+    <div class="index_block">
+<img src="img/yoga_clock.jpg" class = "index_image">
+  <h2>Без ограничений</h2>
+    <ul class = "index_list">
+        <li class="index_li">30 занятий в неделю.</li>
+        <li class="index_li">Семь дней в неделю: Утро / День / Вечер.</li>
+        <li class="index_li">Мини-группы до 6 человек.</li>
+        <li class="index_li">Удобная запись</li>
+    </ul>
+</div>
+    <div class="index_block">
+<img src="img/yoga_clock.jpg" class = "index_image">
+  <h2>1 занятие</h2>
+    <ul class = "index_list">
+        <li class="index_li">30 занятий в неделю.</li>
+        <li class="index_li">Семь дней в неделю: Утро / День / Вечер.</li>
+        <li class="index_li">Мини-группы до 6 человек.</li>
+        <li class="index_li">Удобная запись</li>
+    </ul>
+</div>
+</div>
 
 <?php
 include_once "footer.html";
