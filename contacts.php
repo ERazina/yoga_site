52fd3dc74e8fe2873959675e97fe85f57e5d031f
Elina <razina_elina@mail.ru>
2016-05-03 15:51:58 +0300

    schedule added
    not completed

--- contacts.php
+++ contacts.php
@@ -5,7 +5,7 @@ include_once "menu.php";
 ?>
 
 
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1998.843140019957!2d30.340657316096816!3d59.934744981874765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x469631a772137e53%3A0x67402419fc66749d!2z0L3QsNCxLiBQ0LXQutC4INCk0L7QvdGC0LDQvdC60LgsIDIxLCDQodCw0L3QutGCLdCf0LXRgtC10YDQsdGD0YDQsywgMTkxMDIz!5e0!3m2!1sru!2sru!4v1461512532222" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31984.476964820686!2d30.268907821843094!3d59.931647514037365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4696311c5642ff95%3A0x8209e9bf3a641067!2z0YAuINCk0L7QvdGC0LDQvdC60LA!5e0!3m2!1sru!2sru!4v1462274295382" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 <?php
 include_once "footer.html";
--- schedule.php
+++ schedule.php
@@ -5,10 +5,162 @@ include_once "menu.php";
 ?>
 
 <h1>Расписание</h1>
-<?php
-echo "Здесь нечто";
+<table id = "tab">
+    <tr>
+        <td class = "td_day">День недели</td>
+        <td>Уровень сложности</td>
+        <td>Время</td>
+        <td>Место</td>
+        <td>Преподаватель</td>
+    </tr>
+        <table class = "inner_tab">
+                <tr>
+                    <td></td>
+                <td>Начальный</td>
+                <td>10-11</td>
+                <td>Зал 2</td>
+                <td>Мария Левицкая</td>
+            </tr>
+                <tr>
+            <td class = "td_day">Понедельник</td>
+                <td>Средний</td>
+                <td>11-11</td>
+                <td>Зал 1</td>
+                <td>Елена Макарова</td>
+            </tr>
+            </tr>
+                <tr>
+            </td><td></td>
+                <td>Продвинутый</td>
+                <td>19-20</td>
+                <td>Зал 1</td>
+                <td>Елена Макарова</td>
+           </tr>
+        </table>
+ <table class = "inner_tab">
+        <tr>
+            <td></td>
+        <td>Начальный</td>
+        <td>10-11</td>
+        <td>Зал 2</td>
+        <td>Мария Левицкая</td>
+    </tr>
+        <tr>
+    <td class = "td_day">Вторник</td>
+        <td>Средний</td>
+        <td>11-11</td>
+        <td>Зал 1</td>
+        <td>Елена Макарова</td>
+    </tr>
+    </tr>
+        <tr>
+    </td><td></td>
+        <td>Продвинутый</td>
+        <td>19-20</td>
+        <td>Зал 1</td>
+        <td>Елена Макарова</td>
+   </tr>
+</table>
+ <table class = "inner_tab">
+        <tr>
+            <td></td>
+        <td>Начальный</td>
+        <td>10-11</td>
+        <td>Зал 2</td>
+        <td>Мария Левицкая</td>
+    </tr>
+        <tr>
 
-?>
+    <td class = "td_day">Среда</td>
+        <td>Средний</td>
+        <td>11-11</td>
+        <td>Зал 1</td>
+        <td>Елена Макарова</td>
+    </tr>
+    </tr>
+        <tr>
+    </td><td></td>
+        <td>Продвинутый</td>
+        <td>19-20</td>
+        <td>Зал 1</td>
+        <td>Елена Макарова</td>
+   </tr>            
+</table>
+   <table class = "inner_tab">
+        <tr>
+            <td></td>
+        <td>Начальный</td>
+        <td>10-11</td>
+        <td>Зал 2</td>
+        <td>Мария Левицкая</td>
+    </tr>
+        <tr>
+    <td class = "td_day">Четверг</td>
+        <td>Средний</td>
+        <td>11-11</td>
+        <td>Зал 1</td>
+        <td>Елена Макарова</td>
+    </tr>
+    </tr>
+        <tr>
+    </td><td></td>
+        <td>Продвинутый</td>
+        <td>19-20</td>
+        <td>Зал 1</td>
+        <td>Елена Макарова</td>
+   </tr>            
+</table>
+   <table class = "inner_tab">
+        <tr>
+            <td></td>
+        <td>Начальный</td>
+        <td>10-11</td>
+        <td>Зал 2</td>
+        <td>Мария Левицкая</td>
+    </tr>
+        <tr>
+
+    <td class = "td_day">Пятница</td>
+        <td>Средний</td>
+        <td>11-11</td>
+        <td>Зал 1</td>
+        <td>Елена Макарова</td>
+    </tr>
+    </tr>
+        <tr>
+    </td><td></td>
+        <td>Продвинутый</td>
+        <td>19-20</td>
+        <td>Зал 1</td>
+        <td>Елена Макарова</td>
+   </tr>
+</table>
+   <table class = "inner_tab">
+        <tr>
+            <td></td>
+        <td>Начальный</td>
+        <td>10-11</td>
+        <td>Зал 2</td>
+        <td>Мария Левицкая</td>
+    </tr>
+        <tr>
+    <td class = "td_day">Суббота</td>
+        <td>Средний</td>
+        <td>11-11</td>
+        <td>Зал 1</td>
+        <td>Елена Макарова</td>
+    </tr>
+    </tr>
+        <tr>
+    </td><td></td>
+        <td>Продвинутый</td>
+        <td>19-20</td>
+        <td>Зал 1</td>
+        <td>Елена Макарова</td>
+   </tr>
+</table>
+</table>
+
 <?php
 include_once "footer.html";
 ?>
